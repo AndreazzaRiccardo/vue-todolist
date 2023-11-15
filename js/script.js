6e400e295e26e8ff3633580821b05ce9f8d99bf7
AndreazzaRiccardo <riccardo.andreazza90@gmail.com>
2023-11-15 19:05:08 +0100

    add: exercise complete

--- js/script.js
+++ js/script.js
@@ -3,10 +3,28 @@ const {createApp} = Vue;
 createApp({
     data () {
         return {
-
+            toDoList: [],
+            toDo: "",
         }
     },
-    methods: {
+    created () {
 
+    },
+    methods: {
+        addToList: function () {
+            if(this.toDo !== "") {
+                this.toDoList.push({
+                    text: this.toDo,
+                    done: false
+                });
+            }
+            this.toDo = "";
+        },
+        deleteThis: function (index) {
+            this.toDoList.splice(index, 1);
+        },
+        checked: function (index) {
+            this.toDoList[index].done = !this.toDoList[index].done
+        }
     }
 }).mount("#app")
