3422f4abdbeca05868f9f9e2ae6ada012ecbe155
AndreazzaRiccardo <riccardo.andreazza90@gmail.com>
2023-11-16 06:05:48 +0100

    add: done section

--- js/script.js
+++ js/script.js
@@ -5,21 +5,29 @@ createApp({
         return {
             toDoList: [],
             toDo: "",
+            doneList: []
         }
     },
     created () {
        this.addFromStorage(); 
     },
     methods: {
-
+        // Allo start della pagina autocompila i campi richiamando gli oggetti salvati nel local storage
         addFromStorage: function() {
             if(localStorage.getItem("List") !== null){
             this.toDoList = JSON.parse(localStorage.getItem("List"));
             }
+            if(localStorage.getItem("doneList") !== null){
+            this.doneList = JSON.parse(localStorage.getItem("doneList"));
+            }
         },
-
+        // Ad ogni iterazione, esegue il refresh delle key salvate nel local storage
+        refreshStorage: function() {
+            localStorage.setItem("List", JSON.stringify(this.toDoList));
+            localStorage.setItem("doneList", JSON.stringify(this.doneList));
+        },
+        // Aggiunge un nuovo oggetto alla lista
         addToList: function () {
-            
             let item = {
                 text: this.toDo,
                 done: false
@@ -30,15 +38,28 @@ createApp({
             }
             this.toDo = "";
         },
-
-        deleteThis: function (index) {
-            this.toDoList.splice(index, 1);
-            localStorage.setItem("List", JSON.stringify(this.toDoList));
+        // Cancella un'oggetto dalla lista
+        deleteThis: function (myArray, index) {
+            if(!myArray[index].done){
+                this.toDoList.splice(index, 1);
+            } else {
+                this.doneList.splice(index, 1);
+            }
+            this.refreshStorage();
         },
-
-        checked: function (index) {
+        // Sposta un oggetto nella sezione done
+        checkedDone: function (index) {
             this.toDoList[index].done = !this.toDoList[index].done;
-            localStorage.setItem("List", JSON.stringify(this.toDoList));
+            this.doneList.push(this.toDoList[index]);
+            this.toDoList.splice(index, 1);
+            this.refreshStorage();
+        },
+        // Ripristina un'oggetto dalla sezione done
+        notDone: function(index) {
+            this.doneList[index].done = !this.doneList[index].done;
+            this.toDoList.push(this.doneList[index]);
+            this.doneList.splice(index, 1);
+            this.refreshStorage();
         }
     }
 }).mount("#app")
