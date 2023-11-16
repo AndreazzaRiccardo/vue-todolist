a74bc153f5f4ab07357c65db6ac0b89a020900c2
AndreazzaRiccardo <riccardo.andreazza90@gmail.com>
2023-11-16 04:09:50 +0100

    add: local storage

--- js/script.js
+++ js/script.js
@@ -8,23 +8,37 @@ createApp({
         }
     },
     created () {
-
+       this.addFromStorage(); 
     },
     methods: {
+
+        addFromStorage: function() {
+            if(localStorage.key(0) !== null){
+            this.toDoList = JSON.parse(localStorage.getItem("List"));
+            }
+        },
+
         addToList: function () {
+            
+            let item = {
+                text: this.toDo,
+                done: false
+            }
             if(this.toDo !== "") {
-                this.toDoList.push({
-                    text: this.toDo,
-                    done: false
-                });
+                this.toDoList.push(item);
+                localStorage.setItem("List", JSON.stringify(this.toDoList));
             }
             this.toDo = "";
         },
+
         deleteThis: function (index) {
             this.toDoList.splice(index, 1);
+            localStorage.setItem("List", JSON.stringify(this.toDoList));
         },
+
         checked: function (index) {
-            this.toDoList[index].done = !this.toDoList[index].done
+            this.toDoList[index].done = !this.toDoList[index].done;
+            localStorage.setItem("List", JSON.stringify(this.toDoList));
         }
     }
 }).mount("#app")
